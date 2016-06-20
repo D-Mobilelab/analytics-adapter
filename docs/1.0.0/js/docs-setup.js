a07750a6fc4e4acfa691b6d72d40c7870af2d9c8
aureliome <aurelio.me@gmail.com>
2016-06-20 17:22:49 +0200

    Version 1.0.0

---
+++ docs/1.0.0/js/docs-setup.js
@@ -0,0 +1,25 @@
+NG_DOCS={
+  "sections": {
+    "version": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "version",
+      "id": "AnalyticsAdapter",
+      "shortName": "AnalyticsAdapter",
+      "type": "object",
+      "moduleName": "AnalyticsAdapter",
+      "shortDescription": "Adapter for Google Analytics ",
+      "keywords": "accesstype action adapter analytics analyticsadapter app applications assigned attributes category code configurations console custom define defined dimension dimensions doesn enable enabled event example function future general google hit init initialization initialize label list logged logger logging logs menu method methods number object open options pageview pair pass paymenttype phase save saves set setdimension setid slot title track trackevent tracking trackpage true ui undefined user userstatus valuable version"
+    }
+  ],
+  "apis": {
+    "version": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/version/AnalyticsAdapter",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
