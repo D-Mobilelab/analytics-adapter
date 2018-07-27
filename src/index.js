00a898e723f0a765c653efb4a0cbe33581564219
Aurelio Merenda <aurelio.merenda@docomodigital.com>
2018-07-27 17:08:15 +0200

    Installed airbnb-base eslint config and fixed all problems on src/index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 export default {
     dimensions: {},
     enabled: true,
-    logger: { 
-        debug: function(){},
-        log: function(){},
-        info: function(){},
-        warn: function(){},
-        error: function(){}
+    logger: {
+        debug: () => {},
+        log: () => {},
+        info: () => {},
+        warn: () => {},
+        error: () => {}
     },
 
-    init: function(options) {
-        if(options) {
-            if(options.dimensions){
+    init: (options) => {
+        if (options) {
+            if (options.dimensions) {
                 this.dimensions = options.dimensions;
             }
-            if(typeof options.enabled !== 'undefined'){
+            if (typeof options.enabled !== 'undefined') {
                 this.enabled = options.enabled;
             }
 
             // get logger
-            if (options.logger){
+            if (options.logger) {
                 this.logger = options.logger;
             }
         }
@@ -27,94 +27,88 @@ export default {
         this.logger.log('Analytics', 'init', this);
     },
 
-    setId: function(id){
-        if(id){
+    setId: (id) => {
+        if (id) {
             this.logger.log('AnalyticsAdapter', 'set id', id);
 
-            if(this.enabled){
+            if (this.enabled) {
                 window.ga('set', '&uid', id);
             }
         }
     },
 
-    setDimension: function(dimensionObj){
-        let key, slot, value;
-        if(dimensionObj){
-            for(key in dimensionObj){
-                if (Object.prototype.hasOwnProperty.call(dimensionObj, key)) {
-                    slot = this.dimensions[key];
-                    value = dimensionObj[key];
+    setDimension: (dimensionObj) => {
+        let slot, value;
+        if (dimensionObj) {
+            Object.keys(dimensionObj).forEach((key) => {
+                slot = this.dimensions[key];
+                value = dimensionObj[key];
 
-                    this.logger.log('AnalyticsAdapter', 'set dimension', slot, value);
+                this.logger.log('AnalyticsAdapter', 'set dimension', slot, value);
 
-                    if(this.enabled){
-                        window.ga('set', 'dimension' + slot, value);
-                    }
+                if (this.enabled) {
+                    window.ga('set', `dimension${slot}`, value);
                 }
-            }
+            });
         }
     },
 
-    trackPage: function(options){
-        let properties = { 
+    trackPage: (options) => {
+        const properties = {
             hitType: 'pageview'
         };
-        let key, slot, value;
+        let slot, value;
 
-        if(options.page){
+        if (options.page) {
             properties.page = options.page;
         }
-        if(options.title){
+        if (options.title) {
             properties.title = options.title;
         }
-        if(options.dimensions){
-            for(key in options.dimensions){
-                if (Object.prototype.hasOwnProperty.call(options.dimensions, key)) {
-                    slot = this.dimensions[key];
-                    value = options.dimensions[key];
-                    properties['dimension' + slot] = value;
-                }
-            }
+        if (options.dimensions) {
+            Object.keys(options.dimensions).forEach((key) => {
+                slot = this.dimensions[key];
+                value = options.dimensions[key];
+                properties[`dimension${slot}`] = value;
+            });
         }
 
         this.logger.log('AnalyticsAdapter', 'track pageview', properties);
 
-        if(this.enabled){
+        if (this.enabled) {
             window.ga('send', properties);
         }
     },
 
-    trackEvent: function(options){
-        let properties = { 
+    trackEvent: (options) => {
+        const properties = {
             hitType: 'event'
         };
-        let key, slot, value;
+        let slot, value;
 
-        if(options.category){
+        if (options.category) {
             properties.eventCategory = options.category;
         }
-        if(options.action){
+        if (options.action) {
             properties.eventAction = options.action;
         }
-        if(options.label){
+        if (options.label) {
             properties.eventLabel = options.label;
         }
-        if(options.value){
+        if (options.value) {
             properties.eventValue = options.value;
         }
-        if(options.dimensions){
-            for(key in options.dimensions){
-                if (Object.prototype.hasOwnProperty.call(options.dimensions, key)) {
-                    slot = this.dimensions[key];
-                    value = options.dimensions[key];
-                    properties['dimension' + slot] = value;
-                }
-            }
+        if (options.dimensions) {
+            Object.keys(options.dimensions).forEach((key) => {
+                slot = this.dimensions[key];
+                value = options.dimensions[key];
+                properties[`dimension${slot}`] = value;
+            });
         }
 
         this.logger.log('AnalyticsAdapter', 'track event', properties);
 
-        if(this.enabled){
+        if (this.enabled) {
             window.ga('send', properties);
         }
     }
